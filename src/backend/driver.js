a3b7b7edacfb32452608fcb9a24139aa4510f07e
Jan Prochazka <jan.prochazka@gmail.com>
2021-01-03 11:32:47 +0100

    driver.connect, driver.query uses tedious

--- src/backend/driver.js
+++ src/backend/driver.js
@@ -1,43 +1,67 @@
 const _ = require('lodash');
 const stream = require('stream');
-const mssql = require('mssql');
+// const mssql = require('mssql');
+const tedious = require('tedious');
 const driverBase = require('../frontend/driver');
 const MsSqlAnalyser = require('./MsSqlAnalyser');
 const createBulkInsertStream = require('./createBulkInsertStream');
 
+// function extractColumns(columns) {
+//   const mapper = {};
+//   const res = _.sortBy(_.values(columns), 'index').map((col) => ({
+//     ...col,
+//     columnName: col.name,
+//     notNull: !col.nullable,
+//   }));
+
+//   const generateName = () => {
+//     let index = 1;
+//     while (res.find((x) => x.columnName == `col${index}`)) index += 1;
+//     return `col${index}`;
+//   };
+
+//   // const groups = _.groupBy(res, 'columnName');
+//   // for (const colname of _.keys(groups)) {
+//   //   if (groups[colname].length == 1) continue;
+//   //   mapper[colname] = [];
+//   //   for (const col of groups[colname]) {
+//   //     col.columnName = generateName();
+//   //     mapper[colname].push(colname);
+//   //   }
+//   // }
+
+//   for (const col of res) {
+//     if (!col.columnName) {
+//       const newName = generateName();
+//       mapper[col.columnName] = newName;
+//       col.columnName = newName;
+//     }
+//   }
+
+//   return [res, mapper];
+// }
+
 function extractColumns(columns) {
-  const mapper = {};
-  const res = _.sortBy(_.values(columns), 'index').map((col) => ({
-    ...col,
-    columnName: col.name,
-    notNull: !col.nullable,
-  }));
+  const res = columns.map((col) => {
+    const resCol = {
+      columnName: col.colName,
+      dataType: col.type.name.toLowerCase(),
+    };
+    if (col.dataLength) resCol.dataType += `(${col.dataLength})`;
+    return resCol;
+  });
 
-  const generateName = () => {
-    let index = 1;
-    while (res.find((x) => x.columnName == `col${index}`)) index += 1;
-    return `col${index}`;
-  };
-
-  // const groups = _.groupBy(res, 'columnName');
-  // for (const colname of _.keys(groups)) {
-  //   if (groups[colname].length == 1) continue;
-  //   mapper[colname] = [];
-  //   for (const col of groups[colname]) {
-  //     col.columnName = generateName();
-  //     mapper[colname].push(colname);
-  //   }
-  // }
-
-  for (const col of res) {
-    if (!col.columnName) {
-      const newName = generateName();
-      mapper[col.columnName] = newName;
-      col.columnName = newName;
+  const usedNames = new Set();
+  for (let i = 0; i < res.length; i++) {
+    if (usedNames.has(res[i].columnName)) {
+      let suffix = 2;
+      while (res.has(`${res[i].columnName}${suffix}`)) suffix++;
+      res[i].columnName = `${res[i].columnName}${suffix}`;
     }
+    usedNames.add(res[i].columnName);
   }
 
-  return [res, mapper];
+  return res;
 }
 
 /** @type {import('dbgate-types').EngineDriver} */
@@ -45,19 +69,35 @@ const driver = {
   ...driverBase,
   analyserClass: MsSqlAnalyser,
   async connect({ server, port, user, password, database }) {
-    const pool = new mssql.ConnectionPool({
-      server,
-      port,
-      user,
-      password,
-      database,
-      requestTimeout: 1000 * 3600,
-      options: {
-        enableArithAbort: true,
-      },
+    return new Promise((resolve, reject) => {
+      const connection = new tedious.Connection({
+        server,
+
+        authentication: {
+          type: 'default',
+          options: {
+            userName: user,
+            password: password,
+          },
+        },
+
+        options: {
+          encrypt: false,
+          enableArithAbort: true,
+          validateBulkLoadParameters: false,
+          requestTimeout: 1000 * 3600,
+          database,
+          port,
+        },
+      });
+      connection.on('connect', function (err) {
+        if (err) {
+          reject(err);
+        }
+        resolve(connection);
+      });
+      connection.connect();
     });
-    await pool.connect();
-    return pool;
   },
   // @ts-ignore
   async query(pool, sql) {
@@ -67,20 +107,42 @@ const driver = {
         columns: [],
       };
     }
-    const resp = await pool.request().query(sql);
-    // console.log(Object.keys(resp.recordset));
-    // console.log(resp);
-    const res = {};
+    return new Promise((resolve, reject) => {
+      const result = {
+        rows: [],
+        columns: [],
+      };
+      const request = new tedious.Request(sql, (err, rowCount) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+      request.on('columnMetadata', function (columns) {
+        result.columns = extractColumns(columns);
+      });
+      request.on('row', function (columns) {
+        result.rows.push(
+          _.zipObject(
+            result.columns.map((x) => x.columnName),
+            columns.map((x) => x.value)
+          )
+        );
+      });
+      pool.execSql(request);
+    });
+    // const resp = await pool.request().query(sql);
+    // // console.log(Object.keys(resp.recordset));
+    // // console.log(resp);
+    // const res = {};
 
-    if (resp.recordset) {
-      const [columns] = extractColumns(resp.recordset.columns);
-      res.columns = columns;
-      res.rows = resp.recordset;
-    }
-    if (resp.rowsAffected) {
-      res.rowsAffected = _.sum(resp.rowsAffected);
-    }
-    return res;
+    // if (resp.recordset) {
+    //   const [columns] = extractColumns(resp.recordset.columns);
+    //   res.columns = columns;
+    //   res.rows = resp.recordset;
+    // }
+    // if (resp.rowsAffected) {
+    //   res.rowsAffected = _.sum(resp.rowsAffected);
+    // }
+    // return res;
   },
   async stream(pool, sql, options) {
     const request = await pool.request();
